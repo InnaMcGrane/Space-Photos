5c5fe4ab8104ca66a2df95bcf3cd8f6cfdedc36d
InnaMcGrane <innamcgrane@gmail.com>
2025-05-21 07:33:22 +0100

    get rovers data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,21 @@
 import MarsList from "../MarsList/MarsList";
 
 const App = () => {
+    const DEMO_KEY = 'uFj4Cg3acbb9U3BVt3BpDIynZHq32hfKtf12CSjz'
+    const [rovers, setRovers] = useState([])
+    useEffect(() => {
+        /* 
+        
+        get all rovers https://api.nasa.gov/mars-photos/api/v1/rovers/
+        get photos by rover https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/
+        
+        */
+
+      fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${DEMO_KEY}`)
+      .then((responce) => responce.json())
+      .then((data) => setRovers(data.rovers))
+    }, [])
+    
     return <div className="container">
         <MarsList/>
     </div>
